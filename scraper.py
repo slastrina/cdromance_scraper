71b97baf13669cf1bcbf4bd9dfbce41a97df7fd4
Samuel <50973822+slastrina@users.noreply.github.com>
2019-10-10 23:25:54 +1100

    added multiprocessing

--- scraper.py
+++ scraper.py
@@ -1,14 +1,27 @@
+from multiprocessing.dummy import Pool as ThreadPool
+import sys
 import requests
 import os
 from bs4 import BeautifulSoup
 
 output_path = 'output'
+threads = 2  # site limits you after 2
+content_url = 'https://cdromance.com/sony-psp-dlc-list-psp-downloadable-content/'
+
+def get_file(res):
+    url = f'http://dl4.cdromance.com/download.php?file={res["data-filename"]}&id={res["data-id"]}&platform=page&key=2744046125430717546496'
+
+    r = requests.get(url, allow_redirects=True)
+    print(f'{res["data-filename"]}, {len(r.content)} bytes')
+    sys.stdout.flush()
+
+    with open(f'{output_path}/{res["data-filename"]}', 'wb') as f:
+        f.write(r.content)
 
 if not os.path.exists(output_path):
     os.mkdir(output_path)
 
-content_url = 'https://cdromance.com/sony-psp-dlc-list-psp-downloadable-content/'
-
+print(f'Scraping: {content_url}')
 request = requests.get(content_url)
 
 if request.status_code == 404:
@@ -16,11 +29,14 @@ if request.status_code == 404:
 
 results = BeautifulSoup(request.content, 'html.parser').find_all('div', {'id': 'acf-content-wrapper'})
 
-for result in results:
-    print(result['data-filename'], result['data-id'])
+print(f'Queued {len(results)} files for download')
 
-    url = f'http://dl4.cdromance.com/download.php?file={result["data-filename"]}&id={result["data-id"]}&platform=page&key=2744046125430717546496'
-    r = requests.get(url, allow_redirects=True)
+pool = ThreadPool(2)
 
-    with open(f'{output_path}/{result["data-filename"]}', 'wb') as f:
-        f.write(r.content)
+print('Starting')
+results = pool.map(get_file, results)
+
+pool.close()
+pool.join()
+
+print('Finished')
