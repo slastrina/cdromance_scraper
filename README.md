c7888ed5fbc85f1915953245998279f0fd7c485c
Samuel <50973822+slastrina@users.noreply.github.com>
2019-10-11 23:22:53 +1100

    added note regarding download key

--- README.md
+++ README.md
@@ -3,11 +3,18 @@ Simple scraper for cloning the contents of https://cdromance.com/sony-psp-dlc-li
 
 Requires python 3.7
 
-usage:
-
+### Setup:
+Clone Repo
 pip install -r requirements.txt
+Set key in scraper.py (see key retrieval)
+
+### Key Retrieval:
+To retrieve the key, Open chrome developer tool, Perform a download manually and look a the network tab
+find the link starting with download.php? i.e. http://dl4.cdromance.com/download.php?file=ULJS00385.7z&id=144240&platform=page&key=3015679560829197352960&test=4
+Copy the numbers that appear after key= and before the next & symbol, enter that key below this line, failure to do so will result in errors
+
+### Usage:
 python scraper.py
 
-Note: You may need to adjust the key in the URL.
-
+### Notes:
 The tool may look like its hung after the "Starting" statement, but it just outputs the next line after the file has been downloaded
--- scraper.py
+++ scraper.py
@@ -5,10 +5,14 @@ from multiprocessing.dummy import Pool as ThreadPool
 import requests
 from bs4 import BeautifulSoup
 
-output_path = 'D:/Dropbox/Consoles/retail/Sony PSP/DLC'
 threads = 2  # site limits you after 2
+output_path = 'output'
 content_url = 'https://cdromance.com/sony-psp-dlc-list-psp-downloadable-content/'
 
+# To retrieve the key, Open chrome developer tool, Perform a download manually and look a the network tab
+# find the link starting with download.php? i.e. http://dl4.cdromance.com/download.php?file=ULJS00385.7z&id=144240&platform=page&key=3015679560829197352960&test=4
+# Copy the numbers that appear after key= and before the next & symbol, enter that key below this line, failure to do so will result in errors
+key = '3015679560829197352960'  
 
 def get_file(res):
     """
@@ -23,14 +27,19 @@ def get_file(res):
                   f'?file={res["data-filename"]}' \
                   f'&id={res["data-id"]}' \
                   f'&platform=page' \
-                  f'&key=3015679560829197352960'
+                  f'&key={key}'
 
             r = requests.get(url, allow_redirects=True)
-            print(f'{res["data-filename"]}, {len(r.content)} bytes')
-            sys.stdout.flush()
 
-            with open(f'{output_path}/{res["data-filename"]}', 'wb') as f:
-                f.write(r.content)
+            if len(r.content) < 128:
+                print(f'{res["data-filename"]} failed, Reason: Key Expired')
+                sys.stdout.flush()
+            else:
+                print(f'{res["data-filename"]}, {len(r.content)} bytes')
+                sys.stdout.flush()
+
+                with open(f'{output_path}/{res["data-filename"]}', 'wb') as f:
+                    f.write(r.content)
         except Exception as ex:
             print(f'{res["data-filename"]} failed, Reason: {ex}')
 
@@ -44,6 +53,8 @@ if not os.path.exists(output_path):
 
 print(f'Scraping: {content_url}')
 request = requests.get(content_url)
+
+print(request.status_code)
 
 if request.status_code == 404:
     exit(1)
